da3029e2a1f1c8721fff80dc5606eff2cd778a7a
mr2 <nielsrb@outlook.dk>
2019-02-25 21:03:15 +0100

    making player stats and spellbook

--- BloodlineBosses/Assets/Scripts/PlayerController.cs
+++ BloodlineBosses/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    float hMov;
-    float vMov;
+    Vector3 directions;
+    public GameObject prefab;
+    public static readonly Plane plane = new Plane(Vector3.up, Vector3.zero);
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +18,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        hMov = Input.GetAxis("Horizontal");
-        vMov = Input.GetAxis("Vertical");
-        float dt = Time.deltaTime;
-        Vector3 changeVector = new Vector3(hMov, 0f, vMov);
-        changeVector = changeVector * dt * moveSpeed;
-        gameObject.transform.Translate(changeVector);
+
+        GetInput();
+        Move();
+
     }
+
+    private void Move()
+    {
+        this.transform.Translate(directions * moveSpeed * Time.deltaTime);
+    }
+
+    private void GetInput()
+    {
+        directions = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            directions = directions + Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            directions = directions + Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            directions = directions + Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            directions = directions + Vector3.right;
+        }
+    }
+
+    //private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    //{
+    //    return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    //}
 }
+
---
+++ BloodlineBosses/Assets/Scripts/Spells.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spells : MonoBehaviour
+{
+    List<Spell> mageSpellbook = new List<Spell>();
+    Spell fireball1 = new Spell("Fireball", 10, 10f, 10);
+    Spell fireball2 = new Spell("Fireball", 10, 10f, 10);
+    Spell fireball3 = new Spell("Fireball", 10, 10f, 10);
+    Spell fireball4 = new Spell("Fireball", 10, 10f, 10);
+    Spell fireball5 = new Spell("Fireball", 10, 10f, 10);
+    Spell fireball6 = new Spell("Fireball", 10, 10f, 10);
+    Spell fireball7 = new Spell("Fireball", 10, 10f, 10);
+    Spell fireball8 = new Spell("Fireball", 10, 10f, 10);
+    public PlayerStats PlayerStats;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        mageSpellbook.Add(fireball1);
+        mageSpellbook.Add(fireball2);
+        mageSpellbook.Add(fireball3);
+        mageSpellbook.Add(fireball4);
+        mageSpellbook.Add(fireball5);
+        mageSpellbook.Add(fireball6);
+        mageSpellbook.Add(fireball7);
+        mageSpellbook.Add(fireball8);
+
+        print(PlayerStats.player.Name);
+
+        if (PlayerStats.player.Name == "Mage")
+        {
+            PlayerStats.player.Spells = mageSpellbook;
+        }
+        
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public class Spell
+    {
+        private string _name { get; set; }
+        private int _damage { get; set; }
+        private float _range { get; set; }
+        private int _meter { get; set; }
+        public Spell(string name, int damage, float range, int meter)
+        {
+            _name = name;
+            _damage = damage;
+            _range = range;
+            _meter = meter;
+        }
+    }
+}
