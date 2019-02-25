2663c40a91c24481e8271be1897b8b2b3ef976a8
mr2 <nielsrb@outlook.dk>
2019-02-25 01:17:03 +0100

    added player movement

---
+++ BloodlineBosses/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    float hMov;
+    float vMov;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hMov = Input.GetAxis("Horizontal");
+        vMov = Input.GetAxis("Vertical");
+        float dt = Time.deltaTime;
+        Vector3 changeVector = new Vector3(hMov, 0f, vMov);
+        changeVector = changeVector * dt * moveSpeed;
+        gameObject.transform.Translate(changeVector);
+    }
+}
